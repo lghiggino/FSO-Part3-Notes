6e13a50777713ce58a1e4ec55389da3f07686807
lghiggino <lghiggino@gmail.com>
2021-07-25 09:06:54 -0300

    build folder for the frontend

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let notes = require("./notes");
 
 
 const app = express();
+/***MIDDLEWARE***/
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"))
 
-
+/***ROUTES***/
 app.get("/", (request, response) => {
     response.send("<h1>Hello World</h1>")
 })
@@ -37,9 +39,15 @@ app.delete("/api/notes/:id", (request, response) => {
 })
 
 app.put("/api/notes/:id/importance", (request, response) => {
+    //O FRONTEND ESTA INVERTENDO O VALOR DA IMPORTANCIA DA NOTA, PRECISA SUBSTITUIR NO BACKEND, E RETORNAR TODAS AS NOTAS DE VOLTA (REFRESH)
+    console.log()
     const id = request.params.id
-    const note = note.filter(note => note.id === id)
-    console.log(note)
+    const note = notes.filter(note => {
+        if(note.id === id){
+            note = request.body
+        }
+    })
+    response.status(200).json(notes).end()
 })
 
 app.put("/api/notes/:id/date", (request, response) => {
@@ -76,3 +84,5 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+//HEROKU ADDRESS: https://peaceful-crag-14176.herokuapp.com/api/notes
