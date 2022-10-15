e61758867de155bd57a2d3174603304f9dd09ca0
lghiggino <lghiggino@gmail.com>
2022-10-15 17:13:08 -0300

    test and utils + post and delete routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+let notes = require('./fixtures')
+const generateId = require('./utils')
+
 const app = express()
-const notes = require('./fixtures')
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -13,6 +16,38 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note  = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(notes),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
