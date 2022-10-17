8dcc57cf52d9f27b5aaf3a61c1cd8733e099b14b
lghiggino <lghiggino@gmail.com>
2022-10-17 07:34:35 -0300

    cors + fly.io deployment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-let notes = require("./fixtures");
+const cors = require("cors");
 const { generateId, requestLogger, unknownEndpoint } = require("./utils");
+let notes = require("./fixtures");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -54,7 +56,7 @@ app.post("/api/notes", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
