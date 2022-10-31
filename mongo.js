605de73234516c735d19152afee3fae87ca6c929
lghiggino <lghiggino@gmail.com>
2022-10-31 10:34:09 -0300

    lint

--- mongo.js
+++ mongo.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullStackOpen:${password}@cluster1.4wf08kl.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullStackOpen:${password}@cluster1.4wf08kl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected");
+  .then(() => {
+    console.log('connected')
 
     // const note = new Note({
     //   content: "HTML is Easy",
@@ -34,13 +34,13 @@ mongoose
 
     Note.find({}).then((result) => {
       result.forEach((note) => {
-        console.log(note);
-      });
-      mongoose.connection.close();
-    });
+        console.log(note)
+      })
+      mongoose.connection.close()
+    })
   })
   .then(() => {
-    console.log("note saved!");
-    return mongoose.connection.close();
+    console.log('note saved!')
+    return mongoose.connection.close()
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
